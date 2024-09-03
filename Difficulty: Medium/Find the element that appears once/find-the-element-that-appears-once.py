7d9d0bdacf71c8496fca36550e0e5b86719b7d04
sridharan <sridharan.prof@gmail.com>
2024-09-03 19:28:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find the element that appears once/find-the-element-that-appears-once.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+class Solution:
+    def search(self, n, arr):
+        for i in range(0,len(arr)-1, 2):
+            if arr[i] != arr[i+1]:
+                return arr[i]
+        return arr[-1]
+                
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    for tc in range(t):
+        n = int(input())
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        print(ob.search(n, arr))
+
+# } Driver Code Ends
