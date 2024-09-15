73a8d434b9873ae536f32065fac892476e041c32
sridharan <sridharan.prof@gmail.com>
2024-09-15 15:05:08 +0530

    Time: 15 ms (46.54%), Space: 11.6 MB (72.31%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        xor_ = x ^ y
+        return bin(xor_).count("1")
+        
