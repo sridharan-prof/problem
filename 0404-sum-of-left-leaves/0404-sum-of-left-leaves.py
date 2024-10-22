f79e7584d003168b439950bbcbff9c9c9069573d
sridharan <sridharan.prof@gmail.com>
2024-10-22 15:59:21 +0530

    Time: 0 ms (100%), Space: 16.9 MB (14.23%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -8,20 +8,17 @@ class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
         def isLeaf(node):
             return node is not None and node.left is None and node.right is None
-        
-        # Recursive function to traverse the tree
+
         def dfs(node):
             if node is None:
                 return 0
             
             left_sum = 0
-            # Check if the left child is a leaf
+
             if node.left and isLeaf(node.left):
                 left_sum += node.left.val
-            
-            # Recursively sum the left leaves from both left and right children
+
             return left_sum + dfs(node.left) + dfs(node.right)
-        
-        # Start the DFS traversal from the root
+
         return dfs(root)
         
