81b767ab34df016720a8fb37121cb899ff3f8605
sridharan <sridharan.prof@gmail.com>
2024-12-09 20:14:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Insert Interval/insert-interval.py
@@ -0,0 +1,40 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def insertInterval(self, intervals, newInterval):
+        # Code here
+        intervals.append(newInterval)
+        intervals.sort()
+        merged = []
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+        return merged
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        N = int(input())
+        intervals = [list(map(int, input().split())) for i in range(N)]
+        newEvent = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.insertInterval(intervals, newEvent)
+        print('[', end = '')
+        for i in range(len(res)):
+            print('[', end = '')
+            print(str(res[i][0])+','+str(res[i][1]), end = '')
+            print(']', end = '')
+            if i < len(res)-1:
+                print(',', end='')
+        print(']')
+        print("~")
+# } Driver Code Ends
