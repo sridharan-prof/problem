b7b552e4fbf332688cdfde1bff3dd0be79252789
sridharan <sridharan.prof@gmail.com>
2024-12-22 17:03:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in a Row-Column sorted matrix/search-in-a-row-column-sorted-matrix.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+class Solution:
+	def matSearch(self, mat, x):
+		# Complete this function
+		for row in mat:
+		    for ele in row:
+		        if ele == x:
+		            return True
+	    return False
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+
+    t = int(data[0])
+    index = 1
+    for _ in range(t):
+        r = int(data[index])
+        c = int(data[index + 1])
+        index += 2
+        matrix = []
+        for i in range(r):
+            row = list(map(int, data[index:index + c]))
+            matrix.append(row)
+            index += c
+        x = int(data[index])
+        index += 1
+        ob = Solution()
+        if ob.matSearch(matrix, x):
+            print("true")
+        else:
+            print("false")
+        print("~")
+# } Driver Code Ends
