f71c6839c6c6a3a1d83c922690938b57e318d2a1
sridharan <sridharan.prof@gmail.com>
2025-01-13 19:58:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.py
@@ -0,0 +1,46 @@
+
+class Solution:
+    def maxWater(self, arr):
+        # code here
+        left = 0
+        right = len(arr) - 1
+        max_water = 0
+        
+        while left < right:
+            # Calculate the area formed between the two lines
+            height = min(arr[left], arr[right])
+            width = right - left
+            max_water = max(max_water, height * width)
+            
+            # Move the shorter line inward
+            if arr[left] < arr[right]:
+                left += 1
+            else:
+                right -= 1
+        
+        return max_water
+
+
+#{ 
+ # Driver Code Starts
+#Initial template for Python 3
+
+import math
+
+
+def main():
+    t = int(input())
+    while (t > 0):
+
+        arr = [int(x) for x in input().strip().split()]
+        obj = Solution()
+        print(obj.maxWater(arr))
+
+        t -= 1
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
