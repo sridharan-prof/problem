447eb260a97e363ce66f8516c38342393748e12f
sridharan <sridharan.prof@gmail.com>
2025-01-26 17:00:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the first node of loop in linked list/find-the-first-node-of-loop-in-linked-list.py
@@ -0,0 +1,80 @@
+#User function Template for python3
+
+""" Node Class
+    class Node:
+        def __init__(self, data):   # data -> value stored in node
+            self.data = data
+            self.next = None
+"""
+class Solution:
+    def findFirstNode(self, head):
+        #code here
+        if not head or not head.next:
+            return None
+            
+        slow = head
+        fast = head
+            
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+    
+            if slow == fast: 
+                break
+        else:
+            return None  
+            
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+    
+        return slow
+
+#{ 
+ # Driver Code Starts
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def print_list(node):
+    while node:
+        print(node.data, end=' ')
+        node = node.next
+    print()
+
+
+def loop_here(head, tail, position):
+    if position == 0:
+        return
+
+    walk = head
+    for _ in range(1, position):
+        walk = walk.next
+    tail.next = walk
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().split()))
+        k = int(input())
+        head = Node(arr[0])
+        tail = head
+        for value in arr[1:]:
+            tail.next = Node(value)
+            tail = tail.next
+        loop_here(head, tail, k)
+
+        ob = Solution()
+        ans = ob.findFirstNode(head)
+        if (ans == None):
+            print(-1)
+        else:
+            print(ans.data)
+        print("~")
+
+# } Driver Code Ends
