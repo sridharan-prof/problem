71fa40b9eac46ceaad02ed5a6e15749c3873b906
sridharan <sridharan.prof@gmail.com>
2025-01-27 19:33:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/LRU Cache/lru-cache.py
@@ -0,0 +1,57 @@
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, cap):
+        # Initialize the cache and set its capacity
+        self.cache = OrderedDict()
+        self.capacity = cap
+
+    def get(self, key):
+        
+        if key in self.cache:
+            
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1
+
+    def put(self, key, value):
+        if key in self.cache:
+            
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False) 
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def inputLine():
+    return input().strip().split()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        capacity = int(input())
+        cache = LRUCache(capacity)
+
+        queries = int(input())
+        for __ in range(queries):
+            vec = inputLine()
+            if vec[0] == "PUT":
+                key = int(vec[1])
+                value = int(vec[2])
+                cache.put(key, value)
+            else:
+                key = int(vec[1])
+                print(cache.get(key), end=" ")
+        print()
+        print("~")
+
+# } Driver Code Ends
